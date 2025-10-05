2984e67549a831a586e217de4abcf83ffb837336
Matheus Ribeiro <matheusribeirofon20@gmail.com>
2025-10-05 18:29:17 -0300

    Update projetinho.py

--- World I/projetinho.py
+++ World I/projetinho.py
@@ -1,3 +1,11 @@
+def adicionar_tarefa(tarefas, nome_tarefa):
+    tarefa = {"tarefa": nome_tarefa, "completada": False}
+    tarefas.append(tarefa)
+    print(f"Tarefa {nome_tarefa} foi adicionada com sucesso.")
+    return
+
+tarefas = []
+
 while True:
     print("\nMenu do Gerenciador de Lista de Tarefas:\n")
     print("1. Adicionar Tarefas")
@@ -8,6 +16,9 @@ while True:
     print("6. Sair do Programa")
     
     escolha = int(input("Digite o número da opção desejada:"))
-    if escolha == 6:
+    if escolha == 1:
+        nome_tarefa = input("Digite o nome da tarefa que você deseja adiconar: ")
+        adicionar_tarefa(tarefas, nome_tarefa)
+    elif escolha == 6:
         print("Saindo do programa...")
         break
